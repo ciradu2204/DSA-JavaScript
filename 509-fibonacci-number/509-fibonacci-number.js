a65b5b7fdcefda0dbd5e34c661fc365fdde7fdbd
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-08-23 18:14:17 +0200

    Time: 86 ms (69.38%), Space: 42 MB (44.56%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n == 0){
+        return n; 
+    }
+    if(n === 1){
+        return 1; 
+    }
+    let prec1 = 0; 
+    let prec2 = 1; 
+    for(let i=2; i<n; i++){
+       let result = prec1 + prec2; 
+       prec1 = prec2; 
+       prec2 = result; 
+    }
+    return prec1 + prec2; 
+};
+
+/*algorithm
+f(0) = 0
+f(1) = 1
+f(2) = 1 
+f(3) = 3
+f(4)
+
+prev = 0
+prev2 = 1 
+
+1.loop through from 2 to n-1
+2. update prev = prev2 
+3. update prev2 = sum(prev + prev2)
+4. return sum(prev + prev2)
+
+
+**/
