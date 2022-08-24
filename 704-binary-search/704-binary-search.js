46c97610f047fa1ba5c6352b1b376be246ad1e6e
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-08-24 18:07:49 +0200

    Time: 120 ms (14.52%), Space: 44.7 MB (87.10%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    //iterative approach: O(n)
+    /*iterative approach: O(n)
     let start = 0; 
     let end = nums.length-1; 
     while(start <= end){
@@ -18,5 +18,18 @@ var search = function(nums, target) {
             end--; 
         }
     }
-    return -1; 
+    return -1;*/ 
+    //recursive approach
+   return bs(nums, target, 0, nums.length-1); 
 };
+
+function bs(nums, target, start, end){
+    if(start > end) return -1; 
+    let mid = parseInt(start + (end -start)/2); 
+    if(nums[mid] > target){
+        return bs(nums, target, start, mid-1)
+    }else if(nums[mid] < target){
+       return bs(nums, target, mid+1, end)
+    }
+    return mid; 
+}
