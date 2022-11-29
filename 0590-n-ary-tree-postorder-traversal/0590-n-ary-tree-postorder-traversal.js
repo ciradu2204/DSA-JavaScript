d207220257f7c0dd664c0b86aba91f7927601410
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-11-29 11:49:05 +0200

    Time: 1570 ms (5.17%), Space: 62.1 MB (5.17%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
@@ -10,6 +10,9 @@
  * @param {Node|null} root
  * @return {number[]}
  */
+
+/**
+//Recursive solution
 var postorder = function(root) {
     let result = []; 
     postTraversal(root, result)
@@ -26,16 +29,57 @@ var postTraversal = function(root, result){
     result.push(root.val)  
     return result; 
  }
+*/
+//Iterative Solution
+var postorder = function(root) {
+     let map = new Map();  
+    let result = [];   
+    map.set(root, [null, false]); 
+    while(root != null){
+        console.log(root)
+      if(root.children.length == 0 || (map.has(root.children[root.children.length-1]) && map.get(root.children[root.children.length -1])[1] == true )){
+        result.push(root.val); 
+        let value = map.get(root); 
+        value[1] = true; 
+        map.set(root, value)
+        root = value[0]; 
+     }else{
+        for(let i=0; i<root.children.length; i++){
+          if(!map.has(root.children[i])){
+              map.set(root.children[i], [root, false])
+              root = root.children[i]; 
+              break; 
+          }
+        }
+      }
+    }
+
+return result; 
+};
 
 /*
+Throught process
 post traversal: 
- 
 left, right, root
 
  - Keep an array to store the values
  - Check to the left 
  - Check to the right 
  - Add the root
+ 
+post traversal: Iterative solution
 
+         1
+       / | \
+      3  2  4
+    /  \
+   5    6
+
+  parent = null;
+
+  1. while loop 
+  2. Keep track of the parent and visited (hashMap)
+  3. loop through the elements and target the first element which is not visited and make it the root
+  4.   
 
 */
