aafdb2466087f6c5986fd724afa472cbe8e844e0
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-05-11 12:06:14 +0200

    Time: 84 ms (5.80%), Space: 43.6 MB (22.84%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,12 +4,13 @@
  */
 var compose = function(functions) {
 	return function(x) {
-         let prev = x;
+        if(functions.length == 0) return x;
+         let input = x;
          for(let i=functions.length-1; i>=0;i--){
              let fn = functions[i]
-              prev = fn(prev)
+              input = fn(input)
          }
-         return prev;
+         return input;
     }
 };
 
