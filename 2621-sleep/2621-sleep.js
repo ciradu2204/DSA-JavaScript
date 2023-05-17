9d7ebba5f6e9ec4d3cdef971eeb8950dd1a5d61b
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-05-17 11:57:09 +0200

    Time: 62 ms (42.53%), Space: 41.8 MB (57.61%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -2,8 +2,8 @@
  * @param {number} millis
  */
 async function sleep(millis) {
-    return new Promise((resolve) =>{
-        setTimeout(() => {resolve()}, millis);
+    await new Promise((resolve) =>{
+        setTimeout(resolve, millis);
     })
 }
 
