3b05f26c40a193816927b7154f62312cf12f2ca4
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-05-23 13:16:40 +0200

    Time: 184 ms (74.40%), Space: 58.8 MB (43.42%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,61 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-//      let resultArray = []
-//      nums = nums.sort()
-//     for(let i=0; i<nums.length; i++){
-//         if(nums[i] > 0 ) break
-//         if(i > 0 && nums[i] === nums[i-1]){
-//             continue;
-//         }
-//         let pointer1 = i+1; 
-//         let pointer2 = nums.length-1;
-//         while(pointer1 < pointer2){
-//             let result = nums[pointer1] + nums[pointer2] + nums[i]
-//             if( result === 0){
-//                 resultArray.push([nums[i],nums[pointer1], nums[pointer2]])
-//                 pointer1++;
-//                 pointer2--;
-//                 while(nums[pointer1] == nums[pointer1 -1] && pointer1<pointer2){
-//                     pointer1++;
-//                 }
-//             }else if(result > 0){
-//                pointer2--
-//             }else{
-//               pointer1++
-//             }
-            
-//         }
-//     }
-//     return resultArray;
-    
-    const res = [];
-    nums.sort((a,b) => a-b)
-
-    for (let i = 0; i < nums.length; i++) {
-        const a = nums[i];
-        if (a > 0) break;
-        if (i > 0 && a === nums[i - 1]) continue;
-
-        let l = i + 1;
-        let r = nums.length - 1;
-        while (l < r) {
-            const threeSum = a + nums[l] + nums[r];
-            if (threeSum > 0) {
-                r--;
-            } else if (threeSum < 0) {
-                l++;
-            } else {
-                res.push([a, nums[l], nums[r]]);
-                l++;
-                r--;
-                while (nums[l] === nums[l - 1] && l < r) {
-                    l++;
+     let resultArray = []
+     nums = nums.sort((a,b) => a-b)
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] > 0 ) break
+        if(i > 0 && nums[i] === nums[i-1]){
+            continue;
+        }
+        let pointer1 = i+1; 
+        let pointer2 = nums.length-1;
+        while(pointer1 < pointer2){
+            let result = nums[pointer1] + nums[pointer2] + nums[i]
+            if( result === 0){
+                resultArray.push([nums[i],nums[pointer1], nums[pointer2]])
+                pointer1++;
+                pointer2--;
+                while(nums[pointer1] == nums[pointer1 -1] && pointer1<pointer2){
+                    pointer1++;
                 }
+            }else if(result > 0){
+               pointer2--
+            }else{
+              pointer1++
             }
+            
         }
     }
-    return res;
+    return resultArray;
+  
 };
 
 /**
