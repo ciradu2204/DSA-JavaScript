7836a8b0fe49e5e9dfcb9ce5e2d643563e909d9b
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-05-30 22:09:15 +0200

    Time: 1042 ms (5.04%), Space: 41.7 MB (97.99%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+if(s.length == 1 ) return 1;
+let cStr = s; 
+let pointer1 = 0;
+let pointer2 = 1;
+let change = k;
+let map = new Map()
+let max = 0
+
+while(pointer2 < cStr.length){
+  let val1 = cStr[pointer1]
+  let val2 = cStr[pointer2]
+   if(val1 !== val2){
+      if(change > 0){
+        change--;
+        pointer2++
+      }else{
+        let diff = pointer2 - pointer1;
+        max = Math.max(max, diff);
+        pointer1++;
+        pointer2 = pointer1+1;
+        change = k;
+      }   
+  }else{
+       pointer2++ 
+  }
+}
+ while(change > 0 && pointer1>0){
+     pointer1--;
+     change--;
+ }
+    
+ return Math.max(max, pointer2-pointer1)
+
+};
+
+/*
+change = 0
+
+"ABBB"
+ 
+2
+*/
