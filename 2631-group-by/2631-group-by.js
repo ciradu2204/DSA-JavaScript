7d46245fe2e455efffe4a8a0222f8a1872bab901
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-06-13 13:59:57 +0200

    Time: 136 ms (86.18%), Space: 65.8 MB (37.17%) - LeetHub

--- 2631-group-by/2631-group-by.js
+++ 2631-group-by/2631-group-by.js
@@ -3,25 +3,33 @@
  * @return {Array}
  */
 Array.prototype.groupBy = function(fn) {
-    let map = new Map()
-    let result = {}
-    for(let i=0; i<this.length; i++){
-        let id = fn(this[i])
-        let array = []
-        if(map.has(id)){
-            let array = map.get(id)
-            array.push(this[i])
-            map.set(id, array)
-        }else{
-            array.push(this[i])
-            map.set(id,array)
-        }
-    }
+//     let map = new Map()
+//     let result = {}
+//     for(let i=0; i<this.length; i++){
+//         let id = fn(this[i])
+//         let array = []
+//         if(map.has(id)){
+//             let array = map.get(id)
+//             array.push(this[i])
+//             map.set(id, array)
+//         }else{
+//             array.push(this[i])
+//             map.set(id,array)
+//         }
+//     }
     
-    for(const [key, value] of map.entries()){
-         result[key] = value;
-    }
-    return result;
+//     for(const [key, value] of map.entries()){
+//          result[key] = value;
+//     }
+//     return result;
+ return this.reduce((grouped, currentValue) => {
+     let key = fn(currentValue)
+      if(!grouped[key]){
+         grouped[key] = []
+     }
+     grouped[key].push(currentValue)
+     return grouped;
+ }, {})
 };
 
 /**
