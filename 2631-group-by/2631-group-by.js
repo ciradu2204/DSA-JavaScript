57ee121eca6e2fc63fcc42f2e5a04e099b504575
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-06-13 13:35:28 +0200

    Time: 173 ms (5.06%), Space: 74.2 MB (6.31%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Array}
+ */
+Array.prototype.groupBy = function(fn) {
+    let map = new Map()
+    let result = {}
+    for(let i=0; i<this.length; i++){
+        let id = fn(this[i])
+        let array = []
+        if(map.has(id)){
+            let array = map.get(id)
+            array.push(this[i])
+            map.set(id, array)
+        }else{
+            array.push(this[i])
+            map.set(id,array)
+        }
+    }
+    
+    for(const [key, value] of map.entries()){
+         result[key] = value;
+    }
+    return result;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
