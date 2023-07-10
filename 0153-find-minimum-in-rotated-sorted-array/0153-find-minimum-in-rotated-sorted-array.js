c3d6832808a26733baf31baa303404662ad473de
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2023-07-10 21:55:56 +0200

    Time: 72 ms (5.52%), Space: 42.4 MB (12.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,53 +3,44 @@
  * @return {number}
  */
 var findMin = function(nums) {
-//in A sorted array, they would never be a case where the mid point is greater than  right.Therefore 1st condition is nums[left] > nums[right] -> look to the right
-    
-//If the mid point is less than nums[right] then we know that the right side is sorted so we look the left
-   let left = 0; 
-   let right = nums.length -1; 
-   let m = 0;
-    while(left < right){  
-     m = Math.floor((left + right)/2)
-     if(nums[m] > nums[right]){
-         left = m + 1;
-     }else if(nums[m] < nums[right]){
-         right = m
-     }else{
-         nums[left]
-     }
-     
+    let p1 = 0; 
+    let p2 = nums.length -1;
+    while(p1 < p2){
+        let mid = Math.floor((p1+p2)/2)
+        if(nums[mid] > nums[p2]){
+            p1 = mid + 1;
+        }else{
+            p2 = mid; 
+        }
     }
-    
-    return nums[left]
-    
+    return nums[p1]; 
+
 };
 
-/**
-nums = [3, 4, 5, 1, 2]
-Given a sorted rotated array
-minimum element = 1
-             0  1  2  3  4
-Example 3 = [3, 4, 5, 1, 2]
-                      p1  
-                      p2
-    m = (p2 + p1)/2  =2
-    
-    if(m-1 > 0 && nums[m-1] < nums[m] && nums[m-1]< nums[m+1]){
-       p2 = m
-    }else  if(m + 1 < nums.length && nums[m+1] < nums[m] && nums[m+1] < nums[m-1]){
-       p1 = m+1
-    }else{
-      return nums[m]
-    }
-    
-    if()
-brute force = O(n)
-binary search = O(logn)
+
+/* 
+-We need to find pivot element
+ [2, 1]
+  p1 
+  m
+      p2
+ 0 1 2 3 4
+[3,4,5,1,2]
+       p1
+       m
+nums = [4,5,6,7,0, 1,2]
+                p1 m   
+                     p2
+
+          p2
+result= nums[left]
+if(nums[p1] < nums[p2]){
+   return nums[p1]
+}
+midpoint = (p1 +p2)/2
+
+case 1: midpoint > left -> check left
+case 2: midpoint < left -> check right
 
 
-
-
-
-
-**/
+*/
